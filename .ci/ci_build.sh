50d3a053b43a2c2e15cdf68c79ae6d73bb9f34db
Sersch <edeltraud70@gmx.de>
2020-04-10 02:14:25 +0200

    update ci5

--- .ci/ci_build.sh
+++ .ci/ci_build.sh
@@ -43,7 +43,7 @@ elif [[ $CI_NAME == *"mingw64_nt"* ]]; then
 	echo "Number of Cores $NUMBER_OF_PROCESSORS"
 	mkdir build || exit 1
 	cd build
-	cmake -DPLATFORM=${PLATFORM} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G Ninja ../ || exit 2
+	cmake -DPLATFORM=${PLATFORM} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../ || exit 2
 	ninja || exit 3
 	exit 0;
 	exit 1 || { echo "---> Hyperion compilation failed! Abort"; exit 5; }
