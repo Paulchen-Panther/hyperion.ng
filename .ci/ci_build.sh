fd920622d516d52af34c0195bee6069e7742d4ef
Paulchen-Panther <Paulchen-Panter@protonmail.com>
2020-04-13 07:42:13 +0200

    azure pipeline

--- .ci/ci_build.sh
+++ .ci/ci_build.sh
@@ -44,7 +44,7 @@ elif [[ $CI_NAME == *"mingw64_nt"* || "$CI_NAME" == 'windows_nt' ]]; then
 	mkdir build || exit 1
 	cd build
 	cmake -DPLATFORM=${PLATFORM} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../ -G "Visual Studio 15 2017 Win64" || exit 2
-	ninja || exit 3
+	make -j $NUMBER_OF_PROCESSORS || exit 3
 	exit 0;
 	exit 1 || { echo "---> Hyperion compilation failed! Abort"; exit 5; }
 elif [[ "$CI_NAME" == 'linux' ]]; then
