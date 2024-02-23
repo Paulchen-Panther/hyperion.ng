9872782deaee6d02c4065d9f3275432c2c7b3ab5
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2024-02-23 14:49:25 +0100

    Refactor

--- cmake/FindTurboJPEG.cmake
+++ cmake/FindTurboJPEG.cmake
@@ -1,49 +1,66 @@
 #  FindTurboJPEG.cmake
 #  TURBOJPEG_FOUND
-#  TurboJPEG_INCLUDE_DIRS
+#  TurboJPEG_INCLUDE_DIR
 #  TurboJPEG_LIBRARY
 
-if (ENABLE_MF)
-	find_path(TurboJPEG_INCLUDE_DIRS
-		NAMES turbojpeg.h
-		PATHS
-		"C:/libjpeg-turbo64"
-		PATH_SUFFIXES include
-	)
-
-	find_library(TurboJPEG_LIBRARY
-		NAMES turbojpeg turbojpeg-static
-		PATHS
-		"C:/libjpeg-turbo64"
-		PATH_SUFFIXES bin lib
-	)
+if(WIN32)
+	set(jpeg_library "turbojpeg-static")
 else()
-	find_path(TurboJPEG_INCLUDE_DIRS
-		NAMES turbojpeg.h
-		PATH_SUFFIXES include
-	)
-
-	find_library(TurboJPEG_LIBRARY
-		NAMES turbojpeg turbojpeg-static
-		PATH_SUFFIXES bin lib
-	)
+	set(jpeg_library "libturbojpeg turbojpeg")
 endif()
 
-if(TurboJPEG_INCLUDE_DIRS AND TurboJPEG_LIBRARY)
-	include(CheckCSourceCompiles)
-	include(CMakePushCheckState)
+find_path(TurboJPEG_INCLUDE_DIR
+	NAMES
+		turbojpeg.h
+	PATHS
+		"C:/libjpeg-turbo64"
+	PATH_SUFFIXES
+		include
+)
 
-	cmake_push_check_state(RESET)
-	list(APPEND CMAKE_REQUIRED_INCLUDES ${TurboJPEG_INCLUDE_DIRS})
-	list(APPEND CMAKE_REQUIRED_LIBRARIES ${TurboJPEG_LIBRARY})
-
-	check_c_source_compiles("#include <turbojpeg.h>\nint main(void) { tjhandle h=tjInitCompress(); return 0; }" TURBOJPEG_WORKS)
-	cmake_pop_check_state()
-endif()
+find_library(TurboJPEG_LIBRARY
+	NAMES
+		libturbojpeg
+		turbojpeg
+	PATHS
+		"C:/libjpeg-turbo64"
+	PATH_SUFFIXES
+		bin
+		lib
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TurboJPEG
-	FOUND_VAR TURBOJPEG_FOUND
-	REQUIRED_VARS TurboJPEG_LIBRARY TurboJPEG_INCLUDE_DIRS TURBOJPEG_WORKS
-	TurboJPEG_INCLUDE_DIRS TurboJPEG_LIBRARY
+	FOUND_VAR
+		TurboJPEG_FOUND
+	REQUIRED_VARS
+		TurboJPEG_LIBRARY
+		TurboJPEG_INCLUDE_DIR
 )
+
+if(TurboJPEG_FOUND)
+	if(NOT TARGET turbojpeg)
+		add_library(turbojpeg UNKNOWN IMPORTED GLOBAL)
+		set_target_properties(turbojpeg PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES "${TurboJPEG_INCLUDE_DIR}"
+			IMPORTED_LOCATION "${TurboJPEG_LIBRARY}"
+		)
+	endif()
+
+	# libjpeg-turbo version detection from: https://github.com/chengfzy/CPlusPlusStudy/blob/master/cmake/FindTurboJpeg.cmake
+	if(EXISTS "${TurboJPEG_INCLUDE_DIR}/jconfig.h")
+		file(STRINGS "${TurboJPEG_INCLUDE_DIR}/jconfig.h" TurboJpegVersion REGEX "LIBJPEG_TURBO_VERSION ")
+		string(REGEX REPLACE ".*VERSION *\(.*\).*" "\\1" TurboJPEG_VERSION "${TurboJpegVersion}")
+	endif()
+
+	if(TurboJPEG_VERSION)
+		define_property(TARGET PROPERTY TURBOJPEG_VERSION_PROPERTY
+			BRIEF_DOCS "TurboJPEG version property."
+			FULL_DOCS "TurboJPEG version property."
+		)
+
+		set_target_properties(turbojpeg PROPERTIES
+			TURBOJPEG_VERSION_PROPERTY ${TurboJPEG_VERSION}
+		)
+	endif()
+endif()
--- test/jsonchecks/checkschema.py
+++ test/jsonchecks/checkschema.py
@@ -9,8 +9,6 @@ from urllib.request import pathname2url
 def path2url(path):
     return urljoin('file:', pathname2url(path))
 
-print('-- validate json file')
-
 jsonFileName   = sys.argv[1]
 schemaFileName = sys.argv[2]
 
@@ -23,7 +21,8 @@ try:
 			instance = json.load(jsonFile)
 			Draft3Validator(schema, resolver=resolver).validate(instance)
 except Exception as e:
-	print('validation error: '+jsonFileName + ' '+schemaFileName+' ('+str(e)+')')
+	print('Validation error: '+jsonFileName + ' '+schemaFileName+' ('+str(e)+')')
 	sys.exit(1)
 
+print("-- Successfully validate JSON file: '" + path.basename(jsonFileName) + "' against schema '" + path.basename(schemaFileName) + "'")
 sys.exit(0)
