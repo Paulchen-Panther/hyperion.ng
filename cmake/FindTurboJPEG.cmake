85a010887fd8e319caf0016b9999cc14339e03ad
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2025-03-02 22:40:19 +0100

    Run all builds

--- cmake/FindTurboJPEG.cmake
+++ cmake/FindTurboJPEG.cmake
@@ -33,29 +33,10 @@ find_package_handle_standard_args(TurboJPEG
 		TurboJPEG_INCLUDE_DIR
 )
 
-if(TurboJPEG_FOUND)
-	if(NOT TARGET turbojpeg)
-		add_library(turbojpeg UNKNOWN IMPORTED GLOBAL)
-		set_target_properties(turbojpeg PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES "${TurboJPEG_INCLUDE_DIR}"
-			IMPORTED_LOCATION "${TurboJPEG_LIBRARY}"
-		)
-	endif()
-
-	# libjpeg-turbo version detection from: https://github.com/chengfzy/CPlusPlusStudy/blob/master/cmake/FindTurboJpeg.cmake
-	if(EXISTS "${TurboJPEG_INCLUDE_DIR}/jconfig.h")
-		file(STRINGS "${TurboJPEG_INCLUDE_DIR}/jconfig.h" TurboJpegVersion REGEX "LIBJPEG_TURBO_VERSION ")
-		string(REGEX REPLACE ".*VERSION *\(.*\).*" "\\1" TurboJPEG_VERSION "${TurboJpegVersion}")
-	endif()
-
-	if(TurboJPEG_VERSION)
-		define_property(TARGET PROPERTY TURBOJPEG_VERSION_PROPERTY
-			BRIEF_DOCS "TurboJPEG version property."
-			FULL_DOCS "TurboJPEG version property."
-		)
-
-		set_target_properties(turbojpeg PROPERTIES
-			TURBOJPEG_VERSION_PROPERTY ${TurboJPEG_VERSION}
-		)
-	endif()
+if(TurboJPEG_FOUND AND NOT TARGET turbojpeg)
+	add_library(turbojpeg UNKNOWN IMPORTED GLOBAL)
+	set_target_properties(turbojpeg PROPERTIES
+		INTERFACE_INCLUDE_DIRECTORIES "${TurboJPEG_INCLUDE_DIR}"
+		IMPORTED_LOCATION "${TurboJPEG_LIBRARY}"
+	)
 endif()
